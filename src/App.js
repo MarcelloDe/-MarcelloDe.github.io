6a8e7094e49058e1a6ddfa988f99784887489f12
MarcelloDe <59294768+MarcelloDe@users.noreply.github.com>
2020-05-09 17:57:09 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Sidebar from './components/Sidebar';
+import Sidebar from './Components/Sidebar';
 import Landing from './Components/Landing';
 import Experience from './Components/Experience';
 import Education from './Components/Education';
