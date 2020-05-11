9cb52f564a2a595b85755c9afe84fb2fb9d01739
MarcelloDe <59294768+MarcelloDe@users.noreply.github.com>
2020-05-11 15:08:10 -0400

    Update Landing.js

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
-
-<FontAwesomeIcon icon={faCoffee} />
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 class Landing extends Component {
   constructor(props) {
