6ee6f059b57648f990fb6acc387a5e0ee97c5abd
Marcello <de.filippis@yahoo.com>
2022-01-20 23:20:27 -0500

    Updates

---
+++ precache-manifest.3f2d4947e587cfad9c573de070e4be81.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59a175ac3b85752d773dd02b5d307f06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "def8ca25bf42d355a99d",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "59ec584dc13b934f6ec3",
+    "url": "/static/js/2.8df22139.chunk.js"
+  },
+  {
+    "revision": "def8ca25bf42d355a99d",
+    "url": "/static/js/main.1f3d6b62.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
